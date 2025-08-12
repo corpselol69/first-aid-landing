1205af993fb538804067cd5fee15a680e95864de
d33sf0me <katekrokh@gmail.com>
2025-08-12 13:51:10 +0300

    Support done

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,25 +1,28 @@
 export function Support() {
   return (
-  <section className="section bg-white">
-      <div className="container">
-    <h2 className="text-2xl font-semibold">Поддержка</h2>
-    <p className="mt-2 text-muted">Мы в Telegram:</p>
-        <div className="mt-3 flex flex-wrap gap-3">
+    <section className="section">
+      <div className="container uppercase font-display flex flex-col items-center gap-16">
+        <div className="flex flex-col items-center gap-1">
+          <p className="text-2xl font-light">Мы в телеграмме</p>
           <a
-      className="px-4 py-2 rounded-md border border-brand text-brand hover:bg-brand/5"
-      href="https://t.me/pplus_support"
+            className=" text-brand text-4xl font-extrabold"
+            href="https://t.me/podoroZnikRF"
             target="_blank"
             rel="noreferrer"
           >
-      @pplus_support
+            @podoroZnikRF
           </a>
+        </div>
+
+        <div className="flex flex-col items-center gap-1 ">
+          <p className="text-2xl font-light">Поддержка</p>
           <a
-      className="px-4 py-2 rounded-md border border-brand text-brand hover:bg-brand/5"
-      href="https://t.me/podoroZnikRF"
+            className="text-brand text-4xl font-extrabold"
+            href="https://t.me/pplus_support"
             target="_blank"
             rel="noreferrer"
           >
-      @podoroZnikRF
+            @pplus_support
           </a>
         </div>
       </div>
