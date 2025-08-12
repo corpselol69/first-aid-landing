6920556290f0ca8c6d092f196d6a265278152f48
d33sf0me <katekrokh@gmail.com>
2025-08-12 12:20:29 +0300

    delete NoticeBar

--- src/components/NoticeBar.tsx
+++
@@ -1,9 +0,0 @@
-export function NoticeBar() {
-  return (
-    <div className="bg-danger text-white">
-      <div className="container py-2 text-sm text-center">
-        Может работать некорректно в некоторых браузерах. Рекомендуем: Яндекс, Chrome, Safari, Edge. Требуется Android 6+.
-      </div>
-    </div>
-  );
-}
